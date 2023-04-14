92aa317a9958cfc2d00b82047dd27156f70e17f5
Hal1903 <hahikeyuamomo@gmail.com>
2023-04-14 18:27:54 -0400

    test

---
+++ components/homeStack.js
@@ -0,0 +1,18 @@
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation'
+import Home from '../screens/home';
+// import ReviewDetails
+
+const screens = {
+    Home: {
+        screen: Home,
+
+    },
+    ReviewDetails: {
+        screen: ReviewDetails
+    }
+}
+
+const HomeStack = createStackNavigator(screen)
+
+export default createAppContainer(HomeStack);
